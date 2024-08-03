290e55da921f29d46c92c79fd3723930dcc5687f
Naiyer Kalim <7639533+naiyerkalim@users.noreply.github.com>
2024-08-04 03:00:52 +0530

    Added All files

---
+++ notes.js
@@ -0,0 +1,54 @@
+//level01: 
+ //Selector:
+   //The selector is used to target HTML elements on which you want to apply styles. It could be an element type, class, ID, or a combination of these.
+ //Property:
+   //The property is the specific style attribute you want to set for the selected element. It could be things like color, font-size, margin, padding, etc.
+ //Value:
+   //The value is the specific setting for the property. It could be a color, size, length, etc. 
+
+//Absolute Unit:
+  //In CSS, an "absolute unit" refers to a unit of measurement that is fixed and does not depend on other properties such as font size or the dimensions of the viewport.
+//Pixel(px):
+  //The pixel is the smallest unit of measurement on a digital display.
+//Point(pt):
+  //Points are a unit of measurement typically used in print. One point is equal to 1/72 of an inch. 
+
+//TextDecoration:
+  //underline, overline, linethrough...etc
+//TextTransform:
+  //uppercase, lowercase..etc
+
+//Line height:
+//font property:
+  //font-weight
+//font property:
+  //font style
+//font family:
+
+//Box Model
+  // Each HTML element is considered a rectangular box, and the box model defines how these boxes are structured, including their content, padding, border, and margin. 
+//Display:
+ //In CSS, the display property is used to control how an HTML element is displayed. It determines the type of box the element generates and how it interacts with other elements in the layout. 
+//Inline-block
+  //inline-block is a value for the display property in CSS. It combines features of both inline and block elements, allowing an element to flow like an inline element while maintaining block-level properties.
+
+//Relative Units
+  //In CSS, relative units are used to specify a length or size relative to some other property. Relative units are particularly useful for creating designs that are more adaptable and responsive to different screen sizes and devices.
+  //em
+   //The em unit is relative to the font-size of the nearest parent or the element itself. If no font-size is specified, the em is relative to the font-size of the closest ancestor with a specified font-size.
+  //rem
+   //The rem unit is similar to em but is always relative to the root element's font-size. This ensures that the size is consistent across the entire document.
+  //%
+   //The percentage unit is relative to the property it is applied to. For example, if you set the width of an element to 50%, it will be half the width of its containing element.
+  //vw and vh:
+   //Viewport width (vw) and viewport height (vh) are relative to the size of the viewport (the browser window). 1vw is equal to 1% of the viewport width, and 1vh is equal to 1% of the viewport height.
+
+//Position Property:
+  //position property is used to specify the positioning method of an element. It determines how an element is placed within its containing element or the document. The position property can take several values:
+
+//Z-index
+  //The z-index property in CSS is used to control the stacking order of positioned elements along the z-axis. 
+    //It determines the order in which elements are rendered on top of each other when they overlap. Elements with a higher z-index value will appear in front of elements with a lower z-index value.
+
+   
+
